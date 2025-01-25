719cf0d587c4ed1863e3486bc077fd42e2524c79
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-01-25 19:08:21 +0100

    added navbar to the home page

---
+++ toeslag.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once "./includes/Database.php";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toeslag.nl</title>
+    <link rel="stylesheet" href="./toeslag.css">
+</head>
+<body>
+
+    <div class="navbar">
+        <img src="./images/BD logo.png" alt="">
+        <div class="home">
+            <ion-icon name="home"></ion-icon>
+            <a href="./toeslag.php">Home</a>
+        </div>
+        <div class="login">
+            <ion-icon name="person"></ion-icon>
+            <a href="./login.php">Inloggen</a>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
